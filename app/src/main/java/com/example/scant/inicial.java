9d6fdd56180537f07d35f8033bf550f3063cbd6a
jhami <venturap330@gmail.com>
2022-10-27 00:08:47 -0400

    1parte

---
+++ app/src/main/java/com/example/scant/inicial.java
@@ -0,0 +1,57 @@
+package com.example.scant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class inicial extends AppCompatActivity {
+
+    Button btnScan;
+    EditText TXTresultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        btnScan = findViewById(R.id.btnScan);
+        TXTresultado = findViewById(R.id.TXTresultado);
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                IntentIntegrator intergrador= new IntentIntegrator(inicial.this);
+                intergrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                intergrador.setPrompt("Codigo de barras");
+                intergrador.setCameraId(0);
+                intergrador.setBeepEnabled(true);
+                intergrador.setBarcodeImageEnabled(true);
+                intergrador.initiateScan();
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null){
+            if (result.getContents() == null){
+                Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                TXTresultado.setText(result.getContents());
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
